23c76a9d29fb1f414df3cf19c0868d33db880c94
Haashir Sanaa <103138405+haashirr@users.noreply.github.com>
2023-09-18 08:25:12 +0530

    Time: 0 ms (100%), Space: 6.2 MB (6.46%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -4,13 +4,14 @@ public:
         if(n<=1) {
             return n;
         }
-        vector<int>dp(n+1,-1);
-        dp[0]=0;
-        dp[1]=1;
-
-        for(int i=2;i<=n;i++) {
-            dp[i] = dp[i-1]+dp[i-2];
+        int c;
+        int a = 0;
+        int b = 1;
+        for(int i=1;i<n;i++) {
+            c = a+b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return c;
     }
 };
