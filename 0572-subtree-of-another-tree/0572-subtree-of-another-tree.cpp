49c4d3a26587a661b59187c3461f645e1e051138
Haashir Sanaa <103138405+haashirr@users.noreply.github.com>
2023-09-20 09:08:22 +0530

    Time: 18 ms (63.83%), Space: 29.1 MB (16.41%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,23 +11,25 @@
  */
 class Solution {
 public:
-        bool dfs(TreeNode*root,TreeNode*target)
-    {
-     if(root==NULL && target==NULL)
-     return true;
-    if(root==NULL|| target==NULL)
-     return false;
-     if(root->val!=target->val)
-     return false;
-    return dfs(root->left,target->left) && dfs(root->right,target->right);
-     
+    bool dfs(TreeNode* root, TreeNode* target) {
+        if(root==NULL && target==NULL) {
+            return true;
+        }
+        if(root==NULL || target==NULL) {
+            return false;
+        }
+        if(root->val!=target->val) {
+            return false;
+        }
+        return dfs(root->left,target->left) && dfs(root->right,target->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(!root)
+        if(!root) {
             return false;
-        if(dfs(root, subRoot))
+        }
+        if(dfs(root,subRoot)) {
             return true;
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        }
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
     }
-    
 };
