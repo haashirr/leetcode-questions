8f69b4816793d4ec38ce335fde02c565e238b108
Haashir Sanaa <103138405+haashirr@users.noreply.github.com>
2023-09-27 12:52:03 +0530

    Time: 28 ms (73.02%), Space: 21.1 MB (33.58%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mp;
+        for(auto i:strs) {
+            string word = i;
+            sort(word.begin(),word.end());
+            mp[word].push_back(i);
+        }
+        for(auto i:mp) {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
