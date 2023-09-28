163b183cacb66bdc7d24543d0aef004b6db767cf
Haashir Sanaa <103138405+haashirr@users.noreply.github.com>
2023-09-28 18:32:27 +0530

    Time: 0 ms (100%), Space: 7.4 MB (16.36%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,15 +3,16 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+        //reverse
+     
+            reverse(matrix.begin(),matrix.end());
+        
         //transpose
-        for(int i=0;i<m;i++) {
-            for(int j=i;j<n;j++) {
+        for(int i=0;i<matrix.size();i++) {
+            for(int j=i;j<matrix[i].size();j++) {
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        //reverse
-        for(int i=0;i<m;i++) {
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
+        
     }
 };
