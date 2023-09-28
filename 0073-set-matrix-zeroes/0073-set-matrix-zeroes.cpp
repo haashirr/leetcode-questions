40e1c84a977d6fd66b1b5ec8e6146820a7f57f39
Haashir Sanaa <103138405+haashirr@users.noreply.github.com>
2023-09-28 18:49:24 +0530

    Time: 13 ms (33.11%), Space: 13.9 MB (6.06%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool firstrow=false;
+        bool firstcol = false;
+        //step 1 -> check for first row and col
+        for(int i=0;i<matrix.size();i++) {
+            for(int j=0;j<matrix[0].size();j++) {
+                if(matrix[i][j]==0) {
+                    if(i==0) {
+                        firstrow=true;
+                    }
+                    if(j==0) {
+                        firstcol=true;
+                    }
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        //step 2 -> replace inner matrix
+        for(int i=1;i<matrix.size();i++) {
+            for(int j=1;j<matrix[0].size();j++) {
+                if(matrix[i][0]==0 || matrix[0][j]==0) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(firstrow) {
+            for(int j=0;j<matrix[0].size();j++) {
+                matrix[0][j]=0;
+            }
+        }
+        if(firstcol) {
+            for(int i=0;i<matrix.size();i++) {
+                matrix[i][0]=0;
+            }
+        }
+    }
+};
