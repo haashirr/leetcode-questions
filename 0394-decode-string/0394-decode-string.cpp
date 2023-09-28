f2265cfcab557f4888fd0a80d959f7276cfc9874
Haashir Sanaa <103138405+haashirr@users.noreply.github.com>
2023-09-28 16:02:57 +0530

    Time: 0 ms (100%), Space: 7 MB (30.49%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string>st;
+        stack<int>count;
+        string str="";
+        int k=0;
+        for(auto i:s) {
+            if(isdigit(i)) {
+               k=k*10+i-'0'; 
+            }
+            else if(i=='[') {
+                count.push(k);
+                k=0;
+                st.push(str);
+                str="";
+            }
+            else if(i==']') {
+                int c = count.top();
+                count.pop();
+                string x = st.top();
+                st.pop();
+                for(int j=0;j<c;j++) {
+                    x+=str;
+                }
+                str=x;                   
+            }
+            else {
+                str+=i;
+            }
+        }
+        return str;
+    }
+};
